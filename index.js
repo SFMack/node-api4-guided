faa4a5f0d88a89448d7d4c0972b7c02a89873529
SFMack <sfcamack@icloud.com>
2020-05-13 20:22:54 -0700

    update app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
+console.log("port:", port);
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
